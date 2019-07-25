9f4ff36b83d216da0421d04dde9eddfaa6c86ea9
Marc-Daniel Ortega <globulon@gmail.com>
2019-07-25 01:40:08 +0100

    Add Prism Co Cartesian

--- src/main/scala/fp/optics/data/PrismInstances.scala
+++ src/main/scala/fp/optics/data/PrismInstances.scala
@@ -1,13 +1,32 @@
 package fp.optics.data
 
-import cats.arrow.Profunctor
-import fp.optics.Prism
-import fp.optics.syntax.prism._
+import fp.optics._
 import fp.optics.syntax.either._
+import fp.optics.syntax.prism._
 
 private[data] trait PrismInstances {
-  implicit final def prismProfunctor[A, B]: Profunctor[Prism[A, B, ?,?]] = new Profunctor[Prism[A, B, ?,?]] {
+  implicit final def prismProfunctor[A, B]: CoCartesian[Prism[A, B, ?,?]] = new CoCartesian[Prism[A, B, ?,?]] {
     override def dimap[S, T, U, V](fab: Prism[A, B, S, T])(f: U ⇒ S)(g: T ⇒ V): Prism[A, B, U, V] =
       prism( plus(g)(identity[A])  compose fab.`match` compose f)(g compose fab.build)
+
+    override def left[S, T, C]: Prism[A, B, S, T] ⇒ Prism[A, B, S + C, T + C] = p ⇒ prism[A, B, S + C, T + C] {
+      either[S, C, T + C + A] {
+        s ⇒ plus[T, A, T + C, A](Left(_))(identity)(p.`match`(s))
+      } {
+        c ⇒ Left(Right(c))
+      }
+    } {
+      b ⇒ Left(p.build(b))
+    }
+
+    override def right[S, T, C]: Prism[A, B, S, T] ⇒ Prism[A, B, C + S, C + T] = p ⇒ prism[A, B, C + S, C + T] {
+      either[C, S, C + T + A] {
+        c ⇒ Left(Left(c))
+      } {
+        s ⇒ plus[T, A, C + T, A](Right(_))(identity)(p.`match`(s))
+      }
+    } {
+      b ⇒ Right(p.build(b))
+    }
   }
 }
--- src/main/scala/fp/optics/syntax/Eithers.scala
+++ src/main/scala/fp/optics/syntax/Eithers.scala
@@ -4,7 +4,12 @@ import fp.optics._
 
 private[syntax] trait Eithers {
   final def plus[A, B, C, D]: (A ⇒ C) ⇒ (B ⇒ D) ⇒ A + B ⇒ C + D = f ⇒ g ⇒ {
-    case Left(a)  ⇒ Left(f(a))
+    case Left(a) ⇒ Left(f(a))
     case Right(b) ⇒ Right(g(b))
+  }
+
+  final def either[A, B, C]: (A ⇒ C) ⇒ (B ⇒ C) ⇒ A + B ⇒ C = f ⇒ g ⇒ {
+    case Left(a) ⇒ f(a)
+    case Right(b) ⇒ g(b)
   }
 }
